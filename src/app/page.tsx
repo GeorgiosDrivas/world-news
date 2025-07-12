bf62911349422f8f1db5967c1e3cd7a3c99508c6
GeorgiosDrivas <drivasgeorgios0@gmail.com>
2025-07-12 20:45:03 +0300

    Search icon image and more styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import Image from "next/image";
+
 export default function Home() {
   return (
     <div
@@ -5,7 +9,12 @@ export default function Home() {
       className="flex flex-col items-center justify-center h-screen"
     >
       <h1>Essential news, globally filtered.</h1>
-      <input type="text" placeholder="Search..." />
+      <div className="relative">
+        <input name="search" type="text" placeholder="Search..." />
+        <div id="search-btn">
+          <Image src="/search.svg" alt="Search icon" width={25} height={25} />
+        </div>
+      </div>
     </div>
   );
 }
